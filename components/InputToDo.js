7d49f76aab2af82cd9d9584fa7f086ef19c99b26
MedJoggins <mehdi.a.escudero@gmail.com>
2020-12-04 09:10:33 +0100

    champs controlé, et button disabled done

--- components/InputToDo.js
+++ components/InputToDo.js
@@ -2,13 +2,39 @@ import React from 'react';
 import {Block, Input, Button} from 'galio-framework';
 
 class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+      buttonDisabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(text) {
+    if (text === '') {
+      this.setState({input: text, buttonDisabled: true});
+    } else {
+      this.setState({input: text, buttonDisabled: false});
+    }
+  }
+
+  handleSubmit() {
+    this.setState({input: '', buttonDisabled: true});
+    // this.props.onAddToDo(this.state.input)
+  }
+
   render() {
     return (
       <Block row>
         <Block flex={8}>
           <Input
+            onChangeText={(e) => this.handleChange(e)}
             multiline
-            placeholder="Décriver votre demande ..."
+            placeholder="Quelle est votre tâche ?"
+            value={this.state.input}
             style={{
               justifyContent: 'flex-start',
               alignItems: 'flex-start',
@@ -19,14 +45,15 @@ class Todo extends React.Component {
         </Block>
         <Block flex={2}>
           <Button
+            disabled={this.state.buttonDisabled}
             onlyIcon
             icon="rightcircle"
             iconFamily="antdesign"
             iconSize={30}
-            color="primary"
+            color={this.state.buttonDisabled ? 'lightgrey' : 'primary'}
             iconColor="#fff"
             style={{width: 40, height: 40}}
-            onPress={() => this.props.onAddToDo()}></Button>
+            onPress={() => this.handleSubmit()}></Button>
         </Block>
       </Block>
     );
